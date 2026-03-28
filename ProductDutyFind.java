110ebe1760373b1af394f6655d1f9ab79be1645a
KurkchiSergey <153356949+KurkchiSergey@users.noreply.github.com>
2026-03-28 07:34:15 +0300

    Delete ProductDutyFind.java

--- ProductDutyFind.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class ProductDutyFind {
-    public static int findProductDuty(int price,int weigth){
-        int amount = price/100 + (weigth*100);
-        return  amount;
-    }
-}
